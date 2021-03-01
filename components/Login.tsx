fd157bca4778d940286b74882dc84f4c828d6f40
geed255 <g33ky.me@pm.me>
2021-03-01 15:29:52 +0300

    feat: Login Form Layout 🤟
    Refs #7

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,28 +3,20 @@ import Link from 'next/link';
 
 import styles from './../styles/form.module.scss';
 
-export default function SignUp() {
+export default function Login() {
   return (
     <div className={styles.formCard}>
-      <h2 className={styles.formCardTitle}>Sign Up</h2>
+      <h2 className={styles.formCardTitle}>Login</h2>
       <form className={styles.form}>
         <div className={styles.inputField}>
-          <label htmlFor="username">Username</label>
+          <label htmlFor="username">Username/Email</label>
           <input type="text" name="username" id="username" />
-        </div>
-        <div className={styles.inputField}>
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
         </div>
         <div className={styles.inputField}>
           <label htmlFor="pwd">Password</label>
           <input type="password" name="pwd" id="pwd" />
         </div>
-        <div className={styles.inputField}>
-          <label htmlFor="cpwd">Confirm Password</label>
-          <input type="password" name="cpwd" id="cpwd" />
-        </div>
-        <input type="submit" value="create account" />
+        <input type="submit" value="login" />
       </form>
       <span className={styles.or}>Or</span>
       <button className={styles.googleBtn}>
@@ -33,9 +25,9 @@ export default function SignUp() {
         <span>Google</span>
       </button>
       <span className={styles.link}>
-        Have an account?{' '}
+        Don't have an account yet?{' '}
         <span className={styles.linkItem}>
-          <Link href="/auth?authType=login">Login Here</Link>
+          <Link href="/auth?authType=signup">Create One Here</Link>
         </span>
       </span>
     </div>
